d0122baf58191e9ac5c927e3bce6d772a3d82b83
avijeet <a@avijeets.com>
2016-05-17 15:52:35 -0400

    initial commit of basics

---
+++ SwiftBasics.playground/Pages/VariablesAndConstants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//vars
+
+var str = "Hello, playground"
+
+str = "Hello, world!"
+
+var number = 10
+
+number = 20
+
+// constants
+
+let language = "Swift"
+
+// Naming conventions
+// rule 1: spaces not allowed
+
+let programminglanguage = "Objective-C"
+
+//rule 2: use camelCase
+
+let favoriteProgrammingLanguage = "Swift"
+
---
+++ SwiftBasics.playground/Pages/types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,57 @@
+//: [Previous](@previous)
+
+///////////////////////////////////////
+////////             Strings       ////
+///////////////////////////////////////
+
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+let street = "West Street"
+
+let concatenatedAddress = country + "," + state + ", " + city
+
+//: [Next](@next)
+// string interpolation
+
+let interpolatedAddress = "\(country), \(state)), \(city)"
+
+let streetAddress = "\(222) \(street)"
+
+//integers
+
+////////////////////////////////////////
+////////         Integers      ////////
+///////////////////////////////////////
+
+let favoriteProgrammingLanguage = "Swift"
+let year = 2014 //int
+
+////////////////////////////////////
+////  Floating Point Numbers   ////
+///////////////////////////////////
+
+var version = 2.0 // double
+
+
+////////////////////////////////////////
+////////         Boolean       ////////
+///////////////////////////////////////
+
+
+let isAwesome = true
+
+////////////////////////////////////
+////        Type Safety        ////
+//////////////////////////////////
+
+var someString = ""
+//someString  = 12.0
+
+
+let bestPlayer: String = "Michael Jordan"
+let averagePointsPerGame: Double = 30.1
+let yearOfRetirement: Int = 2003
+let hallOfFame: Bool = true
+
+
